0392b715c394f01f21be97a5eee37ba302ffa51a
Oreno-Sora <hiroshi.kamiyabar@gmail.com>
2024-02-06 20:21:07 +0900

    5章課題の提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重と身長は定数
+const weight = 68;        // 体重(Kg)
+const height = 1.7;       // 身長(m)
+
+// BMI(体格指数)を求める
+let bmi = weight / (height * height);
+
+// BMIの値を出力する
+console.log(bmi);
+
+// EOF
