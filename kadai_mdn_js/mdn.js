071ebd8c6285463de4d352c65fcc25d6e94810eb
Oreno-Sora <hiroshi.kamiyabar@gmail.com>
2024-02-10 10:45:44 +0900

    【MDN】本日の月の値を修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
 // 今日の日付を取り出す
-const today = new Date();
-const currentYear = today.getFullYear();
-const currentMonth = today.getMonth();
-const currentDay = today.getDate();
+let now = new Date();
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth() + 1;
+const currentDay = now.getDate();
 
 // 取り出した西暦4桁年＋月日を下記フォーマットで出力する
 console.log(currentYear + '年' + currentMonth+ '月' + currentDay+ '日');
